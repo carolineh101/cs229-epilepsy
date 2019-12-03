f97b555192657d725190b3fe8edb59ea2c65d294
carolineh101 <cho19@stanford.edu>
2019-12-03 11:18:56 -0800

    Add results of CNN on raw data.

--- util.py
+++ util.py
@@ -12,7 +12,7 @@ from keras.models import Sequential
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, f1_score, make_scorer
-from sklearn.model_selection import GridSearchCV, GroupKFold, cross_val_predict, cross_validate
+from sklearn.model_selection import GridSearchCV, GroupKFold, GroupShuffleSplit, cross_val_predict, cross_validate
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.utils import shuffle
 
@@ -22,16 +22,14 @@ K = 10
 
 # Hyperparameter search grids.
 CNN_PARAM_GRID = {
-    'filter': [64],
     'window': [3, 5, 7],
-    'pool': [3],
     'dropout': [0.1, 0.2, 0.5],
     'epochs': [25],
     'batch_size': [5]
 }
 KNN_PARAM_GRID = {'n_neighbors': [3, 5, 7, 9]}
 SREG_PARAM_GRID = {'C': [0.1, 0.5, 1, 5]}
-PARAM_GRID_DICT = {'cnn': CNN_PARAM_GRID, 'knn': KNN_PARAM_GRID, 'sreg': SREG_PARAM_GRID}
+PARAM_GRID_DICT = {'knn': KNN_PARAM_GRID, 'sreg': SREG_PARAM_GRID}
 
 
 def parse_data(multiclass=True, normalize=True):
@@ -63,6 +61,12 @@ def parse_data(multiclass=True, normalize=True):
     return X, y, ids, chunks
 
 
+def split_data(X, y, groups, seed=0):
+    # Split data 80/20 train/test (for CNN only), respecting groups (ids).
+    train, test = next(GroupShuffleSplit(random_state=seed).split(X, y=y, groups=groups))
+    return X[train], y[train], groups[train], X[test], y[test], groups[test]
+
+
 def get_scoring_metrics(classes, multiclass=True):
     # Generate scoring function dict (accuracy, macro F1, and F1 per class).
     scoring = {'accuracy': 'accuracy', 'f1_macro': 'f1_macro'}
@@ -77,7 +81,7 @@ def get_scoring_metrics(classes, multiclass=True):
     return scoring
 
 
-def create_cnn_model(input_shape, num_classes, filter, window, pool, dropout):
+def create_cnn_model(input_shape, num_classes, window, dropout, filter=64, pool=3):
     # Adapted from "Sequence classification with 1D convolutions" in keras.io/getting-started/sequential-model-guide.
     model = Sequential()
     model.add(Conv1D(filter, window, activation='relu', input_shape=input_shape))
@@ -92,13 +96,13 @@ def create_cnn_model(input_shape, num_classes, filter, window, pool, dropout):
     return model
 
 
-def get_estimator(model_type, seed, input_shape, num_classes):
+def get_estimator(model_type, seed, input_shape, num_classes, window=3, dropout=0.5):
     if model_type == 'sreg':
         return LogisticRegression(random_state=seed, solver='lbfgs', multi_class='multinomial', max_iter=5000)
     elif model_type == 'knn':
         return KNeighborsClassifier()
     elif model_type == 'cnn':
-        return KerasClassifier(build_fn=create_cnn_model, input_shape=input_shape, num_classes=num_classes, filter=64, window=3, pool=3, dropout=0.5)
+        return KerasClassifier(build_fn=create_cnn_model, input_shape=input_shape, num_classes=num_classes, window=window, dropout=dropout)
 
 
 def evaluate_model(X, y, groups, classes, model_type, scoring, k=K, seed=0, multiclass=True):
@@ -106,26 +110,23 @@ def evaluate_model(X, y, groups, classes, model_type, scoring, k=K, seed=0, mult
     This function evaluates a given model using nested cross-validation.
     Adapted from scikit-learn.org/stable/auto_examples/model_selection/plot_nested_cross_validation_iris.html.
     """
-    assert model_type in PARAM_GRID_DICT, 'model_type currently only supports sreg, knn, and cnn.'
+    assert model_type in PARAM_GRID_DICT, 'model_type currently only supports sreg and knn.'
     param_grid = PARAM_GRID_DICT[model_type]
-    clf = get_estimator(model_type, seed, (X.shape[1], 1), len(classes))
-
-    # Run nested cross-validation.
+    estimator = get_estimator(model_type, seed, (X.shape[1], 1), len(classes))
+    # Run inner fold of CV.
     shuffled_X, shuffled_y, shuffled_groups = shuffle(X, y, groups, random_state=seed)
     inner_cv = GroupKFold(n_splits=k)
     outer_cv = GroupKFold(n_splits=k)
-    clf = GridSearchCV(estimator=clf, param_grid=param_grid, cv=inner_cv, iid=False)
+    clf = GridSearchCV(estimator, param_grid, cv=inner_cv, iid=False)
     clf.fit(shuffled_X, y=shuffled_y, groups=shuffled_groups)
-
-    # Calculate scores.
+    # Run outer fold of CV.
     cv_scores = cross_validate(
         clf, shuffled_X, y=shuffled_y, groups=shuffled_groups, cv=outer_cv, scoring=scoring,
         fit_params={'groups': shuffled_groups}, return_train_score=True)
+    # Calculate scores and confusion matrix.
     train_scores = {metric : np.mean(cv_scores['train_%s' % metric]) for metric in scoring}
     test_scores = {metric : np.mean(cv_scores['test_%s' % metric]) for metric in scoring}
     fold_accuracy = cv_scores['test_accuracy']
-
-    # Calculate confusion matrix.
     y_pred = cross_val_predict(
         clf, shuffled_X, y=shuffled_y, groups=shuffled_groups, cv=outer_cv,
         fit_params={'groups': shuffled_groups})
@@ -133,15 +134,36 @@ def evaluate_model(X, y, groups, classes, model_type, scoring, k=K, seed=0, mult
     return train_scores, test_scores, fold_accuracy, cm
 
 
-def plot_fold_accuracy(fold_accuracy, k=K):
+def evaluate_cnn(X_train, y_train, groups_train, X_test, y_test, classes, scoring, k=K, seed=0, multiclass=True):
+    """
+    This function identifies the best parameters for a CNN using non-nested cross-validation
+    (based on accuracy) and re-trains the best model on the full train set.
+    """
+    estimator = get_estimator('cnn', seed, (X_train.shape[1], 1), len(classes))
+    # Run CV.
+    cv = GroupKFold(n_splits=k)
+    clf = GridSearchCV(
+        estimator, CNN_PARAM_GRID, cv=cv, iid=False, scoring=scoring, refit='accuracy', return_train_score=True)
+    clf.fit(X_train, y=y_train, groups=groups_train)
+    # Evaluate model.
+    acc = clf.score(X_test, y=y_test)
+    y_pred = clf.predict(X_test)
+    f1_per_class = f1_score(y_test, y_pred, average=None, labels=classes)
+    f1_macro = np.mean(np.array(f1_per_class))
+    cm = confusion_matrix(y_test, y_pred)
+    return clf.cv_results_, acc, f1_macro, f1_per_class, cm
+
+
+def plot_fold_accuracy(fold_accuracy, k=K, fname=None):
     plt.bar(range(1, k + 1), fold_accuracy)
     plt.title('Per-Fold Accuracy')
     plt.ylabel('Accuracy')
     plt.xlabel('Fold')
     plt.show()
+    if fname: plt.savefig(fname + '_fold_acc.png')
 
 
-def visualize_confusion_matrix(cm, classes):
+def visualize_confusion_matrix(cm, classes, fname=None):
     # Adapted from scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html.
     plt.imshow(cm, interpolation='nearest', cmap='Blues')
     plt.title('Confusion Matrix')
@@ -160,6 +182,7 @@ def visualize_confusion_matrix(cm, classes):
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.show()
+    if fname: plt.savefig(fname + '_cm.png')
 
 
 ########## BELOW FUNCTIONS FOR DATA VISUALIZATION ##########
