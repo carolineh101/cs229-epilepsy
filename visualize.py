40452204cec132be197d2e0618f98b6bac3e3a57
tchainzzz <tchang97@stanford.edu>
2019-12-02 22:03:54 -0800

    HMM baseline

--- visualize.py
+++ visualize.py
@@ -4,6 +4,21 @@ import matplotlib.pyplot as plt
 import argparse
 import scipy.fftpack
 
+def visualize_aggregate_fft(df, t=0.01):
+    agg = df.groupby('y').mean().T
+    n = len(agg)
+    print(agg.shape)
+    for colname in agg.columns:
+        xf = np.linspace(t, 1.0/(2.0*t), n/2)[1:]
+        yf = scipy.fftpack.fft(agg.loc[:, colname])
+        plt.plot(xf, 2.0/n * np.abs(yf[1:n//2]))
+    plt.legend(agg.columns)
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Microvolts (μV)")
+    plt.xlim(1, 20)
+    plt.show()
+    
+
 def sample_eegs_from_class(df, class_label, n=1, **kwargs):
     x = list(range(len(df.columns) - 2))
     indices = np.random.choice(list(df[df['y'] == class_label].index), n, replace=False)
@@ -17,8 +32,8 @@ def visualize(df, index, fft=False, t=0.01):
     for i in index:
 
         y = df.iloc[i][1:-1].astype('int8') # this removes the patient ID and the label
+        color = ['r', 'b', 'c', 'g', 'y']
         if fft:
-            color = ['r', 'b', 'c', 'g', 'y']
             x = np.linspace(0.0, n*t, n)
             yf = scipy.fftpack.fft(y)
 
@@ -26,7 +41,7 @@ def visualize(df, index, fft=False, t=0.01):
             plt.plot(xf, 2.0/n * np.abs(yf[:n//2]), color[df.iloc[i][-1] - 1])
         else:
             plt.xlabel("time (in 1/178 s increments; total = 1s)")
-            plt.plot(x, y)
+            plt.plot(x, y, color[df.iloc[i][-1] - 1])
             plt.legend(["Row {}, y={}".format(i, df.iloc[i][-1]) for i in index])
     plt.show()
 
